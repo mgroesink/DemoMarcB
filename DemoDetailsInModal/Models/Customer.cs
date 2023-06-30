49ea5f40cf085461d00623518ce2948f9989c627
mgroesink <mroesink@rocvantwente.nl>
2023-06-30 13:23:43 +0200

    Add project files.

---
+++ DemoDetailsInModal/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoDetailsInModal.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
